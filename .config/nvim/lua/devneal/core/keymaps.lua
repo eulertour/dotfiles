81e356be22b2efec503a56c1209c0f4a40186084
Devin Neal <devin@eulertour.com>
2025-05-29 12:18:53 -0400

    changes

--- .config/nvim/lua/devneal/core/keymaps.lua
+++ .config/nvim/lua/devneal/core/keymaps.lua
@@ -3,12 +3,7 @@ vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode" })
 
 -- vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear swarch highlights", silent = true })
 -- vim.keymap.set("n", "<C-c>", ":NoiceDismiss<CR>", { desc = "Dismiss noice message" })
-vim.keymap.set(
-	"n",
-	"<C-c>",
-	":nohl<CR>:NoiceDismiss<CR>",
-	{ desc = "Clear search highlights and noice messages", silent = true }
-)
+vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search highlights and noice messages", silent = true })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines up" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines down" })
@@ -24,10 +19,10 @@ vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 vim.keymap.set("n", "<leader>cw", ":w<CR>", { desc = "Save current buffer" })
 vim.keymap.set("n", "<leader>cwa", ":wa<CR>", { desc = "Save all buffers" })
 vim.keymap.set("n", "<leader>ce", ":e<CR>", { desc = "Edit current file" })
-vim.keymap.set("n", "<leader>cq", ":q<CR>", { desc = "Quit current file" })
 vim.keymap.set("n", "<leader>cx", ":x<CR>", { desc = "Quit and save current file" })
-vim.keymap.set("n", "<leader>cc", ":qa<CR>", { desc = "Close all buffers" })
-vim.keymap.set("n", "<leader>cwc", ":wqa<CR>", { desc = "Save and close all buffers" })
+vim.keymap.set("n", "<leader>cq", ":q<CR>", { desc = "Quit current file" })
+vim.keymap.set("n", "<leader>cqa", ":qa<CR>", { desc = "Close all buffers" })
+vim.keymap.set("n", "<leader>cwqa", ":wqa<CR>", { desc = "Save and close all buffers" })
 
 -- tabs
 vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
